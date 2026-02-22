a9b3489764ecf92d21b5bd226dfbcc6f1c74df24
Clinic Admin <gt.workfiles@gmail.com>
2026-02-22 16:07:07 +0200

    chore: remove unused sanity admin and configs, keep cms.ts as local data source

--- src/app/admin/[[...index]]/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { NextStudio } from 'next-sanity/studio'; // Виправлено шлях тут
-import config from '../../../../sanity.config';
-
-export default function AdminPage() {
-  return <NextStudio config={config} />;
-}
